7892b41961016a67308006d98bfc6ac052e82dec
Leilay123 <leilayates2002@gmail.com>
2025-11-12 13:38:09 -0600

    wip 3 test

--- index.js
+++ index.js
@@ -9,22 +9,59 @@ $(() => {
 
 function addNewTweets() {
   const $tweets = streams.home.map((tweet) => {
-    const $tweet = $('<div class="tweet"></div>');
-    // change below, need to make user clickable (in it's own tag)
-    // make message it's own tag as well
-    // need timestamps under each tweet
-    // user needs a click handler inside of function
+  const $tweet = $('<div class="tweet"></div>');
 
-    const text = `@${tweet.user}: ${tweet.message}`;
+    // adds the username and message
+    const $user = $(`<span class="user">@${tweet.user}</span>`);
+    const $message = $(`<span class="message">${tweet.message}</span>`);
 
-    $tweet.text(text);
+    // Create timestamp with Moment.js
+    const $timestamp = $('<div class="timestamp"></div>');
+    $timestamp.text(moment(tweet.timestamp).format('MMMM Do YYYY, h:mm:ss a'));
+
+    // Assemble tweet
+    $tweet.append($user, ': ', $message, $timestamp);
 
     return $tweet;
   });
 
-  $tweetsDiv.append($tweets);
-};
+  // Add new tweets above the older ones
+  $tweetsDiv.prepend($tweets);
+}
+
 // .html - use to clear elements
+
 addNewTweets(); // must have new tweets on page but not remove older tweets
 
-});
+// need to connect the function to a button that in the index.html file
+// or I can try my hand at jquery again, whatever works
+  const $refreshButton = $('<button class="refresh-button">Show more tweets</button>');
+
+  $page.prepend($refreshButton);
+
+// tweets are at the bottom, I want them a the top...
+
+  $refreshButton.on('click', () => {
+    $tweetsDiv.empty(); // clear existing tweets
+    // add new tweets, how do I add them over the older tweets?
+
+  });
+
+  // need to add a timestamp to each tweet, example April 24, 2024 3:15pm
+  //  timestamps should be in it's own tag with the class timestamp
+  // the doc says use Moment.js, for the timestamp format
+  // do I use date math or time math?
+
+  // $('.tweet').each(function() {
+  // const $tweet = $(this);
+  // const tweetText = $tweet.text();
+  // const $timestamp = $('<div class="timestamp"></div>');
+  // $timestamp.text(moment(tweet.timestamp).format('MMMM D, YYYY h:mm A'));
+  // $tweet.append($timestamp);
+  // });
+
+  console.log("addNewTweets() called");
+  console.log(streams.home);
+
+
+})
