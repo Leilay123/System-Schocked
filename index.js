a9fe4929cf0878de0f2133020b2a6bdbae8b19c0
Leilay123 <leilayates2002@gmail.com>
2025-11-13 11:25:07 -0600

    fixed disappering tweet issue

--- index.js
+++ index.js
@@ -5,49 +5,44 @@ $(() => {
 
   $page.append($tweetsDiv);
 
-// add twtter to page logic as a function
+// need to create a function that adds new tweets to the top of the tweet list
+
+let lastDisplayedIndex = 0;
 
 function addNewTweets() {
-  const $tweets = streams.home.map((tweet) => {
-  const $tweet = $('<div class="tweet"></div>');
+  // get only new tweets
+  const newTweets = streams.home.slice(lastDisplayedIndex);
 
-    // // adds the username and message
-    
+  // update index to the new end
+  lastDisplayedIndex = streams.home.length;
+
+  // create and prepend tweet elements
+  const $tweets = newTweets.map(tweet => {
+    const $tweet = $('<div class="tweet"></div>');
     const $user = $(`<span class="user">@${tweet.user}</span>`);
     const $message = $(`<span class="message">${tweet.message}</span>`);
-
-    // Create timestamp with Moment.js
-
     const $timestamp = $('<div class="timestamp"></div>');
     $timestamp.text(moment(tweet.timestamp).format('MMMM Do YYYY, h:mm:ss a'));
 
-    // Assemble tweet
     $tweet.append($user, ': ', $message, $timestamp);
-
     return $tweet;
   });
 
-  // Add new tweets above the older ones
+  // add new tweets to the top
   $tweetsDiv.prepend($tweets);
 }
 
-// .html - use to clear elements
+// Initial load
+addNewTweets();
 
-addNewTweets(); // must have new tweets on page but not remove older tweets
+// Create and attach the refresh button
+const $refreshButton = $('<button class="refresh-button">Show more tweets</button>');
+$page.prepend($refreshButton);
 
-// need to connect the function to a button that in the index.html file
-// or I can try my hand at jquery again, whatever works
-  const $refreshButton = $('<button class="refresh-button">Show more tweets</button>');
-
-  $page.prepend($refreshButton);
-
-// tweets are at the bottom, I want them a the top...
-
-  $refreshButton.on('click', () => {
-    $tweetsDiv.empty(); // clear existing tweets
-    // add new tweets, how do I add them over the older tweets?
-
-  });
+// When clicked, add only new tweets
+$refreshButton.on('click', () => {
+  addNewTweets();
+});
 
   // need to add a timestamp to each tweet, example April 24, 2024 3:15pm
   //  timestamps should be in it's own tag with the class timestamp
@@ -61,9 +56,5 @@ addNewTweets(); // must have new tweets on page but not remove older tweets
   // $timestamp.text(moment(tweet.timestamp).format('MMMM D, YYYY h:mm A'));
   // $tweet.append($timestamp);
   // });
-
-  console.log("addNewTweets() called");
-  console.log(streams.home);
-
 
 })
